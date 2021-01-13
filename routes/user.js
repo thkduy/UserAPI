736efb6a379eeb799b2295da25f039743e782a06
Khanh Duy <thkduy123@gmail.com>
2021-01-13 17:39:01 +0700

    update route get game

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
+const ObjectId = require('mongodb').ObjectId;
 const UserModel = require('../model/User');
 const RoomModel = require('../model/Room');
 const Step = require('../model/Step');
 const Message = require('../model/Message');
 
 router.get('/rank', async (req, res) => {
-    const users = await UserModel.find({},function (err, result) {
+    const fields = {name:1, email:1, avatar:1, accountType:1, point: 1, totalWin: 1, numOfMatches:1};
+    const users = await UserModel.find({}, fields, function (err, result) {
         if (err) {
             res.status(400).send(err);
         }
@@ -28,6 +30,8 @@ router.get('/get-all-game-by-userid', async (req, res)=>{
 router.get('/get-game-by-gameid', async (req, res)=>{
     const idRoom = req.query.id; 
     const roomData = await RoomModel.find({roomId:idRoom})
+                                        .populate({path: 'player1', select: 'name avatar'})
+                                        .populate({path: 'player2', select: 'name avatar'})
                                         .populate({path: 'steps'})
                                         .populate({path: 'messages',populate: {path: 'owner', select: 'name avatar'}});;
     res.status(200).json({ data: roomData });
